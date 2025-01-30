ed64a650c287ceaa3667c57d96230484064010cf
sushanthumiitech <rahulgudu2003@gmail.com>
2025-01-30 11:43:23 +0530

    fetching documents

--- src/app/actions/notesActions.ts
+++ src/app/actions/notesActions.ts
@@ -3,10 +3,10 @@ import { ID } from "appwrite";
 export async function addNote(content: string): Promise<Note> {
   const newNote = { content: content };
   const response = await databases.createDocument(
-      "6795bf360025d6ede5ec",
-      "6795bf48000e185e7d57",
-      ID.unique(),
-      newNote
+    "6795bf360025d6ede5ec",
+    "6795bf48000e185e7d57",
+    ID.unique(),
+    newNote
   );
 
   const note = {
@@ -16,4 +16,19 @@ export async function addNote(content: string): Promise<Note> {
   };
 
   return note;
+}
+
+export async function getNotes(): Promise<Note[]> {
+  const response = await databases.listDocuments(
+    "6795bf360025d6ede5ec",
+    "6795bf48000e185e7d57"
+  );
+  console.log(response.documents);
+  const notes: Note[] = response.documents.map((doc) => ({
+    $id: doc.$id,
+    $createdAt: doc.$createdAt,
+    content: doc.content,
+  }));
+
+  return notes;
 }
