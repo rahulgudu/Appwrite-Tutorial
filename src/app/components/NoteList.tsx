f50549b9d46c663d5d80ff6b24a6dea9b660c662
sushanthumiitech <rahulgudu2003@gmail.com>
2025-02-09 12:07:45 +0530

    Realtime

--- src/app/components/NoteList.tsx
+++ src/app/components/NoteList.tsx
@@ -1,9 +1,32 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { deleteNode } from "../actions/notesActions";
+import { client } from "@/utils/appwrite";
 
 export default function NoteList({ initialNotes }: { initialNotes: Note[] }) {
   const [notes, setNotes] = useState<Note[]>(initialNotes);
+
+  useEffect(() => {
+    const channel =
+      "databases.6795bf360025d6ede5ec.collections.6795bf48000e185e7d57.documents";
+
+    const unsubscribe = client.subscribe(channel, (res) => {
+      const eventType = res.events[0];
+      console.log(eventType);
+      const changeNote = res.payload as Note;
+
+      if (eventType.includes("create")) {
+        setNotes((prevNotes) => [...prevNotes, changeNote]);
+      }
+      if (eventType.includes("delete")) {
+        setNotes((prevNotes) =>
+          prevNotes.filter((note) => note.$id !== changeNote.$id)
+        );
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const handleDelete = async (noteId: string) => {
     const element = document.getElementById(noteId);
